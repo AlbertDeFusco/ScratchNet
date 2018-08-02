cc7b6ec8c0cae7a59b5e5846a179e8468e37ccf6
Albert DeFusco <albert.defusco@me.com>
2018-08-01 21:28:20 -0400

    working code

---
+++ scratchnet/losses.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function
+import numpy as np
+
+class CrossEntropy(object):
+    def __call__(self, y, T):
+        return -(T * np.log(y) + (1 - T) * np.log(1 - y)).mean()
+
+    def gradient(self, y, T):
+        return (y - T) / y.shape[0]
