fix CrossEntropy gradient method.  During backprop assumes sigmoid is activation on final layer - therefore when sigmoid is final layer, gradient was being multiplied by sigmoid gradient twice.

scratchnet/losses.py
@@ -5,4 +5,4 @@ class CrossEntropy(object):
         return -(T * np.log(y) + (1 - T) * np.log(1 - y)).mean()
 
     def gradient(self, y, T):
-        return (y - T) / y.shape[0]
+        return (y - T) / (y * (1 - y)) / y.shape[0]