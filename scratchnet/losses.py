fix potentially fatal flaw in SoftMax loss function involving pandas.get_dummies

scratchnet/losses.py
@@ -25,16 +25,17 @@ class CrossEntropyWithLogits(object):
         return (s - T) / y.shape[0]
 
 class SoftmaxCrossEntropyWithLogits(object):
-
     def __call__(self, y, T):
         ## y is logits from previous layers and
         ## T is the multiclass labels and should range in value 0 to k
         ## with k + 1 equal to total number of unique labels
         ym = y.max(1)[:, np.newaxis]
         z = y-ym
-        return -((get_dummies(T) * z).sum(1) - logsumexp(z, 1)).mean(0)
+        zt = z[np.arange(T.shape[0]), T]
+        return -(zt - logsumexp(z, 1)).mean(0)
     def gradient(self, y, T):
         ym = y.max(1)[:, np.newaxis]
         eym = np.exp(y-ym)
         sm = eym / eym.sum(1)[:,np.newaxis]
-        return (sm - get_dummies(T)).values / y.shape[0]
+        sm[np.arange(T.shape[0]), T] -= 1
+        return sm / y.shape[0]